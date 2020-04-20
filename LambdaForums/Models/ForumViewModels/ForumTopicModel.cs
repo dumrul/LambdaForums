22aec91b88b94f3232a6a7c20a96ad13cd08549a
Mahmut Dumrul <dumrulmahmut@yahoo.com>
2020-04-20 18:21:29 +0300

    Post service, forum views

---
+++ LambdaForums/Models/ForumViewModels/ForumTopicModel.cs
@@ -0,0 +1,14 @@
+﻿using LambdaForums.Models.PostViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Models.ForumViewModels
+{
+    public class ForumTopicModel
+    {
+        public ForumListingModel Forum { get; set; }
+        public IEnumerable<PostListingModel> Posts { get; set; }
+    }
+}
