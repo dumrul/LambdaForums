b1da93d9bbad4b20ce63ea1540a3c5587680939a
dumrul <dumrulmahmut@yahoo.com>
2020-05-03 12:57:30 -0700

    Home controller create

---
+++ LambdaForums/Models/HomeViewModels/HomeIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using LambdaForums.Models.PostViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Models.HomeViewModels
+{
+    public class HomeIndexModel
+    {
+        public string SearchQuery { get; set; }
+        public IEnumerable<PostListingModel> LatestPosts { get; set; }
+    }
+}
